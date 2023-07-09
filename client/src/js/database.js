dfaacf706ad8830dd2a32e55fceaf946a5dfac54
itsrayito <rayito.aguirre94@gmail.com>
2023-07-09 01:23:06 -0500

    database addition

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,34 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDB = async (content) => {
+  try {
+    console.log('Post to the database');
+    const jateDb = await openDB('jate', 1 );
+    const tx = jateDb.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    const request = store.add({ value: content });
+    const result = await request;
+    console.log(' data saved to the database', result);
+  } catch (error) {
+    console.error('putDBb not implemented');
+  }
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  try {
+    console.log('GET all from the database');
+    const jateDB = await openDB('jate', 1);
+    const tx = jateDB.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+    const request = store.getAll();
+    const result = await request;
+    console.log('result.value', result);
+    return result;
+  } catch (error) {
+    console.error('getDb not implemented');
+  }
+};
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,28 +3,62 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      
-    ],
+  path: path.resolve(__dirname, 'dist'),
+},
+plugins: [
 
-    module: {
-      rules: [
-        
-      ],
+  new HtmlWebpackPlugin({
+    template: './index.html',
+    title: 'Webpack Plugin',
+  }),
+  new MiniCssExtractPlugin(),
+  new InjectManifest({
+    swSrc: './src/sw.js',
+    swDest: 'service-worker.js',
+  }),
+  new WebpackPwaManifest({
+
+    name: 'PWA-text-editor',
+    short_name: 'JATE',
+    description: 'Text editor progressive web app',
+    background_color: '#ffffff',
+    crossorigin: 'use-credentials',
+    icons: [
+      {
+        src: path.resolve('src/assets/icon.png'),
+        sizes: [96, 128, 192, 256, 384, 512]
+      },
+    {
+      src: path.resolve('src/assets/maskable-icon.png'),
+      size: '1024x1024',
+      purpose: 'maskable'
+    }
+    ]
+  }),
+],
+
+module: {
+  rules: [
+    {
+      test: /\.css$/i,
+      use: [MiniCssExtractPlugin.loader, "css-loader"]
     },
-  };
+    {
+      test: /\.m?js$/,
+      exclude: /node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presents: ['@babel/preset-env'],
+        },
+      },
+    },
+  ],
+},
 };
